03f7c76f34a60cede7b3f25b25d7dd2f8d0da0b3
titusn <mail@titusnachbauer.com>
2021-03-08 21:50:43 +0100

    Add Approval test for 1 to 100

---
+++ src/test/java/FizzBuzzApprovalTest.java
@@ -0,0 +1,17 @@
+import com.titusnachbauer.fizzbuzz.FizzBuzz;
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+class FizzBuzzApprovalTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    void givenValues1to100ShouldReturnApprovedValues() {
+        Integer[] values = new Integer[100];
+        for (int i = 0; i < 100 ; i++) {
+            values[i] = i + 1;
+        }
+        Approvals.verifyAll(values, fizzBuzz::getAnswerFor);
+    }
+
+}

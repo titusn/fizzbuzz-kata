683edd5fe0e5451eff2bf7f93b7dec85decc7dc2
titusn <mail@titusnachbauer.com>
2021-03-08 21:08:23 +0100

    Refactor imports

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,17 +1,18 @@
 import com.titusnachbauer.fizzbuzz.FizzBuzz;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
     @Test
     void given1ShouldReturn1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        Assertions.assertEquals("1", fizzBuzz.getAnswerFor(1));
+        assertEquals("1", fizzBuzz.getAnswerFor(1));
     }
 
     @Test
     void given2ShouldReturn2() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        Assertions.assertEquals("2", fizzBuzz.getAnswerFor(2));
+        assertEquals("2", fizzBuzz.getAnswerFor(2));
     }
 }

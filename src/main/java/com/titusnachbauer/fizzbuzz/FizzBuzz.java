eac76cf7157c7c28fd490f83f3f07d20791df714
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2022-02-15 10:26:06 +0100

    First Tests

--- src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package com.titusnachbauer.fizzbuzz;
 
 public class FizzBuzz {
+    public String get(Integer i) {
+        return "1";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,9 +1,19 @@
 import com.titusnachbauer.fizzbuzz.FizzBuzz;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
+    @Test
+    void NothingTest() {
 
+    }
+
+    @Test
+    void FizzBuzz1ShouldReturn1() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Assertions.assertEquals("1", fizzBuzz.get(1));
+    }
 }
 

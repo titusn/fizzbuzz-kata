b7bf33e75fddd17a323b4437e2c3255a909ddcc5
Benoit Viguier <benoit.viguier@nl.abnamro.com>
2022-02-15 10:30:04 +0100

    refactoring tests

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,21 +4,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
-    @Test
-    void NothingTest() {
 
-    }
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    @Test
+    void NothingTest() {}
 
     @Test
     void FizzBuzz1ShouldReturn1() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.get(1));
     }
 
     @Test
     void FizzBuzz2ShouldReturn2() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.get(2));
     }
+
+    // @Test
+    // void FizzBuzz3ShouldReturn3(){
+    //     FizzBuzz fizzBuzz = new FizzBuzz();
+    //     assertEquals("3", actual);
+    // }
 }
 

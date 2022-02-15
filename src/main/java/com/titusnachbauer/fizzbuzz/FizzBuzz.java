27552f7c99b490df98f6ee29ee6ee33242a8c0ea
Benoit Viguier <benoit.viguier@nl.abnamro.com>
2022-02-15 10:28:28 +0100

    fizz 2

--- src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,6 @@ package com.titusnachbauer.fizzbuzz;
 
 public class FizzBuzz {
     public String get(Integer i) {
-        return "1";
+        return i.toString();
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,5 +1,4 @@
 import com.titusnachbauer.fizzbuzz.FizzBuzz;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,7 +12,13 @@ class FizzBuzzTest {
     @Test
     void FizzBuzz1ShouldReturn1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        Assertions.assertEquals("1", fizzBuzz.get(1));
+        assertEquals("1", fizzBuzz.get(1));
+    }
+
+    @Test
+    void FizzBuzz2ShouldReturn2() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("2", fizzBuzz.get(2));
     }
 }
 

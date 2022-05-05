6561d229cbfe19a54045bf129344bd97c1feba39
Bas Dam <bas.dam@nl.abnamro.com>
2022-05-05 10:40:45 +0200

    removed working code

--- src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,5 @@
 package com.titusnachbauer.fizzbuzz;
 
 public class FizzBuzz {
-    public String get(Integer i) {
 
-        if (i % 3 == 0) {
-            return "Fizz";
-        } else if (i.equals(5)) {
-            return "Buzz";
-        }
-
-        return i.toString();
-    }
 }
--- src/test/java/com/titusnachbauer/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/titusnachbauer/fizzbuzz/FizzBuzzTest.java
@@ -6,35 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
-    FizzBuzz fizzBuzz = new FizzBuzz();
-
     @Test
     void NothingTest() {
-    }
-
-    @Test
-    void FizzBuzz1ShouldReturn1() {
-        assertEquals("1", fizzBuzz.get(1));
-    }
-
-    @Test
-    void FizzBuzz2ShouldReturn2() {
-        assertEquals("2", fizzBuzz.get(2));
-    }
-
-    @Test
-    void FizzBuzz3ShouldReturnFizz() {
-        assertEquals("Fizz", fizzBuzz.get(3));
-    }
-
-    @Test
-    void FizzBuzz5ShouldReturnBuzz() {
-        assertEquals("Buzz", fizzBuzz.get(5));
-    }
-
-    @Test
-    void FizzBuzz6ShouldReturnFizz() {
-        assertEquals("Fizz", fizzBuzz.get(6));
     }
 }
 

ff3aa1496893dd2b7b469b33252971cf6f0e875c
Rein van Rossum <rein.van.rossum@nl.abnamro.com>
2022-07-08 14:47:52 +0200

    FizzBuzzList can handle Fizz, Buzz and FizzBuzz

--- src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/titusnachbauer/fizzbuzz/FizzBuzz.java
@@ -1,10 +1,20 @@
 package com.titusnachbauer.fizzbuzz;
 
 public class FizzBuzz {
-    public static String getResult(int i) {
-        if (i == 1) {
-            return Integer.valueOf(i).toString();
+
+    public static String getFizzBuzzList() {
+        StringBuilder list = new StringBuilder();
+        for(int i = 1; i <=100; i++){
+            if (i % 15 == 0) {
+                list.append("\nFizzBuzz");
+            } else if (i % 3 == 0){
+                list.append("\nFizz");
+            } else if (i % 5 == 0){
+                list.append("\nBuzz");
+            } else {
+                list.append("\n").append(Integer.valueOf(i).toString());
+            }
         }
-        return "Fizz";
+        return list.toString();
     }
 }
--- src/test/java/com/titusnachbauer/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/titusnachbauer/fizzbuzz/FizzBuzzTest.java
@@ -5,14 +5,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    String[] fizzBuzzList = FizzBuzz.getFizzBuzzList().split("\n");
+
     @Test
-    void iSayOneYouSayOne(){
-        assertEquals("1", FizzBuzz.getResult(1));
+    void firstFizzBuzzList(){
+        String list = FizzBuzz.getFizzBuzzList();
+        assertEquals("1", list.substring(1,2));
+
     }
     @Test
-    void iSayThreeYouSayFizz() {
-        assertEquals("Fizz", FizzBuzz.getResult(3));
+    void firstNumberFromNumberListIsOne(){
+        assertEquals("1", fizzBuzzList[1]);
+    }
+    @Test
+    void hundredthNumberFromFuzzBizzListIsBuzz(){
+        assertEquals("Buzz", fizzBuzzList[100]);
     }
 
-
+    @Test
+    void thirdNumberFromNumberListIsFizz(){
+        assertEquals("Fizz", fizzBuzzList[3]);
+    }
+    @Test
+    void ninetyninethNumberFromNumberListIsAlsoFizz(){
+        assertEquals("Fizz", fizzBuzzList[99]);
+    }
+    @Test
+    void fifthNumberFromFizzBuzzListIsBuzz(){
+        assertEquals("Buzz", fizzBuzzList[5]);
+    }
+    @Test
+    void fifteenthNumberFromFizzBuzzListisFizzBuzz(){
+        assertEquals("FizzBuzz", fizzBuzzList[15]);
+    }
 }

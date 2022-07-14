99bae00506ce955c7431d0f6b01859671d19a8d1
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2022-07-14 09:25:33 +0200

    Remove all code for demo

--- src/test/java/com/titusnachbauer/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/titusnachbauer/fizzbuzz/FizzBuzzTest.java
@@ -1,41 +1,6 @@
 package com.titusnachbauer.fizzbuzz;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
-    String[] fizzBuzzList = FizzBuzz.getFizzBuzzList().split("\n");
 
-    @Test
-    void firstFizzBuzzList(){
-        String list = FizzBuzz.getFizzBuzzList();
-        assertEquals("1", list.substring(1,2));
-
-    }
-    @Test
-    void firstNumberFromNumberListIsOne(){
-        assertEquals("1", fizzBuzzList[1]);
-    }
-    @Test
-    void hundredthNumberFromFuzzBizzListIsBuzz(){
-        assertEquals("Buzz", fizzBuzzList[100]);
-    }
-
-    @Test
-    void thirdNumberFromNumberListIsFizz(){
-        assertEquals("Fizz", fizzBuzzList[3]);
-    }
-    @Test
-    void ninetyninethNumberFromNumberListIsAlsoFizz(){
-        assertEquals("Fizz", fizzBuzzList[99]);
-    }
-    @Test
-    void fifthNumberFromFizzBuzzListIsBuzz(){
-        assertEquals("Buzz", fizzBuzzList[5]);
-    }
-    @Test
-    void fifteenthNumberFromFizzBuzzListisFizzBuzz(){
-        assertEquals("FizzBuzz", fizzBuzzList[15]);
-    }
 }
